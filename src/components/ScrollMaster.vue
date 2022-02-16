2ea3781424270022f063ca6908ded72bd6fd2e48
Koho Jung <koho.jung@outlook.com>
2022-02-17 07:48:10 +0800

    .

--- src/components/ScrollMaster.vue
+++ src/components/ScrollMaster.vue
@@ -34,23 +34,25 @@ export default {
 		touchmoveEvent(e) {
 			if (!this.startPoint) return;
 
-			const scrollTop = this.scroll.scrollTop; // 距离页面顶部的距离
+			// const scrollLeft = this.scroll.scrollLeft; // 距离页面左边缘的距离
 			const curPoint = this.getPoint(e);
-			const moveY = curPoint.y - this.startPoint.y; // 纵向移动的距离
+			const moveX = curPoint.x - this.startPoint.x; // 纵向移动的距离
 
-			// 下拉
-			if (moveY > 0) {
-				// 如果在顶部,阻止浏览器默认的滚动,避免触发bounce
-				if (scrollTop <= 0) e.preventDefault();
-				// 上拉
-			} else {
-				const scrollHeight = this.scroll.scrollHeight; // 全文区域的高度
-				const clientHeight = this.scroll.clientHeight; // 可见区域的高度
-				const scrollBottom = scrollHeight - clientHeight - scrollTop; // 距离页面底部的距离
+			if (Math.abs(moveX) > 0) e.preventDefault();
 
-				// 如果在底部,阻止浏览器默认的滚动,避免触发ios的bounce效果
-				if (scrollBottom <= 0) e.preventDefault();
-			}
+			// // 右拉
+			// if (moveX > window.innerWidth / 3) {
+			// 	// 如果在左边缘,阻止浏览器默认的滚动,避免触发bounce
+			// 	if (scrollLeft <= 0) e.preventDefault();
+			// 	// 左拉
+			// } else if (moveX < -window.innerWidth / 3) {
+			// 	const scrollWeight = this.scroll.scrollWeight; // 全文区域的宽度
+			// 	const clientWeight = this.scroll.clientWeight; // 可见区域的宽度
+			// 	const scrollRight = scrollWeight - clientWeight - scrollLeft; // 距离页面右边缘的距离
+
+			// 	// 如果在右边缘,阻止浏览器默认的滚动,避免触发ios的bounce效果
+			// 	if (scrollRight <= 0) e.preventDefault();
+			// }
 		}
 	}
 };
@@ -60,7 +62,7 @@ export default {
 .scroll {
 	width: 100%;
 	height: 100%;
-	overflow-y: auto;
+	overflow-x: auto;
 	-webkit-overflow-scrolling: touch;
 }
 </style>
