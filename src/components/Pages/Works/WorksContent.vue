8fd25eb1d70ed732ffde96e6d6ea6c0e9c9a1269
Koho Jung <koho.jung@outlook.com>
2022-02-19 19:55:00 +0800

    Update WorksContent.vue

--- src/components/Pages/Works/WorksContent.vue
+++ src/components/Pages/Works/WorksContent.vue
@@ -92,8 +92,18 @@
         <!-- scroll tip -->
         <div
           v-if="i != myInfo._Works.length - 1"
-          class="breath-line z-40 sm:hidden absolute rounded-full h-1/4 right-0 my-auto inset-y-0 w-1 bg-primary bg-opacity-85"
-        ></div>
+          class="breath-line z-40 sm:hidden flex items-center absolute rounded-full h-1/4 mr-1 right-0 my-auto inset-y-0"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+          </svg>
+        </div>
       </div>
     </div>
   </div>
