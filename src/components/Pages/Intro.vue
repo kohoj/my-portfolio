790911ba1537338841486493d9ef9b09a5766e93
kohoj <koho.jung@outlook.com>
2022-10-18 00:50:50 +0800

    update

--- src/components/Pages/Intro.vue
+++ src/components/Pages/Intro.vue
@@ -9,7 +9,9 @@
         class="rounded-box bg-neutral border-2 card-body max-w-lg p-4"
         style="border-color:#1f1f21"
       >
-        <p class="text-left mb-2 md:text-lg" v-html="myInfo._Detail"></p>
+      <div v-for="(p,i) in myInfo._Detail.split('||')" :key="i">
+        <p class="text-left mb-4 md:text-lg">{{p}}</p>
+      </div>
         <div class="flex flex-wrap">
           <div
             v-for="(item, i) in myInfo._Stack"
